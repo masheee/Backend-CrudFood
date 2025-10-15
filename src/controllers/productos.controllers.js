45aaa4b5556d1a97d01c48ddb68cfbd1eb3e264c
MaxiGT <mazisecundario@gmail.com>
2025-10-14 21:24:25 -0300

    agregamos el controlador para borrar un producto por ID

--- src/controllers/productos.controllers.js
+++ src/controllers/productos.controllers.js
@@ -32,3 +32,32 @@ export const listarProductos = async (req, res) => {
     }
 }
 
+export const obtenerProducto = async (req, res) => {
+    try {
+        console.log(req.params.id)
+        const productoBuscado = await Producto.findById(req.params.id)
+        if(!productoBuscado){
+            return res.status(404).json({mensaje: 'No se encontró el producto'})
+        }
+        res.status(200).json(productoBuscado)
+    } catch (error) {
+        console.error(error)
+        res.status(500).json({mensaje:'Ocurrio un error al listar los productos'})
+    }
+}
+
+export const borrarProducto = async (req, res) => {
+    try {
+        console.log(req.params.id)
+        const productoBuscado = await Producto.findById(req.params.id)
+        if(!productoBuscado){
+            return res.status(404).json({mensaje: 'No se encontró el producto'})
+        }
+        await Producto.findByIdAndDelete(req.params.id)
+        res.status(200).json({mensaje: 'El producto fue eliminado correctamente'})
+    } catch (error) {
+        console.error(error)
+        res.status(500).json({mensaje:'Ocurrio un error al borrar el producto'})
+    }
+}
+
--- src/routes/productos.routes.js
+++ src/routes/productos.routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express"
-import { prueba, crearProducto, listarProductos } from "../controllers/productos.controllers.js";
+import { prueba, crearProducto, listarProductos, obtenerProducto, borrarProducto } from "../controllers/productos.controllers.js";
 
 /*
 GET
@@ -12,6 +12,7 @@ const router = Router()
 
 router.route('/test').get(prueba)
 router.route('/').post(crearProducto).get(listarProductos)
+router.route('/:id').get(obtenerProducto).delete(borrarProducto)
 
 
 export default router;
