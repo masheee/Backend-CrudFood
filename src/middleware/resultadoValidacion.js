01b913192d21a608c6b2c7fff71e9c7e796f6adf
MaxiGT <mazisecundario@gmail.com>
2025-10-28 13:19:47 -0300

    agregamos validaciones en el post de nombreProducto y precio

--- src/middleware/resultadoValidacion.js
+++ src/middleware/resultadoValidacion.js
@@ -1,12 +1,12 @@
-import { validationResult } from "express-validation"
+import { validationResult } from "express-validator";
 
-const resultadoValidacion = (req, res, next) => {
+const resultadoValidacion = (req, res, next)=>{
     const errores = validationResult(req)
-    //ocurrieron errores en la validacion
+    // ocurrieron errores en la validacion
     if(!errores.isEmpty()){
         return res.status(400).json(errores.array())
     }
-    //continuar la ejecucion 
+    //continuar ejecucion
     next()
 }
 
